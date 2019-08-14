2b864b4f12cedc95cabac1f5c932c40ac912ca6c
yeucon02vn <phiphuqn1@gmail.com>
2019-08-14 23:23:38 +0700

    Updates

---
+++ precache-manifest.c2c04a4f809aee94fe857da0321770ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47dc418c37a1d5530b0c",
+    "url": "/Marketplace/static/js/runtime~main.47dc418c.js"
+  },
+  {
+    "revision": "ccd85691c82cbdd9118d",
+    "url": "/Marketplace/static/js/main.ccd85691.chunk.js"
+  },
+  {
+    "revision": "3b7d2554afe4e4da5e90",
+    "url": "/Marketplace/static/js/1.3b7d2554.chunk.js"
+  },
+  {
+    "revision": "ccd85691c82cbdd9118d",
+    "url": "/Marketplace/static/css/main.a8ce0b0b.chunk.css"
+  },
+  {
+    "revision": "3b7d2554afe4e4da5e90",
+    "url": "/Marketplace/static/css/1.b539102c.chunk.css"
+  },
+  {
+    "revision": "84ce4a4f6222fbaa70be0a99258b7a8a",
+    "url": "/Marketplace/index.html"
+  }
+];
